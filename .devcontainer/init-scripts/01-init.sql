1aef3792dd25422ff7d26fadd46c90b74efe6e36
xiaosj22 <xiaoshujie22@sina.cn>
2026-04-21 16:44:29 +0800

    创建容器66

---
+++ .devcontainer/init-scripts/01-init.sql
@@ -0,0 +1,9 @@
+-- 直接修改 postgres 密码（启动自动执行）
+ALTER USER postgres PASSWORD '123456';
+
+-- 自动创建数据库
+CREATE DATABASE reportdb;
+
+-- 自动创建用户
+CREATE USER dbusrreport WITH PASSWORD 'sub';
+GRANT ALL PRIVILEGES ON DATABASE reportdb TO dbusrreport;
