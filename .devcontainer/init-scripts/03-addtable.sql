1c7985127388a0fa4e983a8ce81adc7b570c820f
xiaosj22 <xiaoshujie22@sina.cn>
2026-05-18 18:01:42 +0800

    创建容器74

--- .devcontainer/init-scripts/03-addtable.sql
+++ .devcontainer/init-scripts/03-addtable.sql
@@ -1,13 +1,13 @@
 -- 创建数据库连接
-insert into database_list (DBID, DBALIAS, PROVIDER, DBTYPE, SERVERNAME, DBNAME, DBUSER, DBUSERPSWD, OTHERS, DBNOTE)
+-- 创建数据库连接
+insert into database_list (db_id, DB_ALIAS, PROVIDER, DB_TYPE, db_SERVER_NAME, DB_NAME, DB_USER, DB_USER_PSWD, OTHERS, DB_NOTE)
 values (1, 'DBBMS', 'OLEDB', 'Oracle', 'DBDMS_bzkf', 'DBDMS', '38ePawvPFxXLcetNtZ1jvg==', '4bTNb3DN0cw=', 'needdecode;ischineselang', 'DBDMS');
 
-insert into database_list (DBID, DBALIAS, PROVIDER, DBTYPE, SERVERNAME, DBNAME, DBUSER, DBUSERPSWD, OTHERS, DBNOTE)
+insert into database_list (db_id, DB_ALIAS, PROVIDER, DB_TYPE, db_SERVER_NAME, DB_NAME, DB_USER, DB_USER_PSWD, OTHERS, DB_NOTE)
 values (2, 'DBDMS', 'OLEDB', 'Oracle', 'DBDMS_bzkf', 'DBDMS', 'h4yQuW5kgZmORhBArzwBZA==', '4bTNb3DN0cw=', 'needdecode;ischineselang', 'DBDMS');
 
-insert into database_list (DBID, DBALIAS, PROVIDER, DBTYPE, SERVERNAME, DBNAME, DBUSER, DBUSERPSWD, OTHERS, DBNOTE)
+insert into database_list (db_id, DB_ALIAS, PROVIDER, DB_TYPE, db_SERVER_NAME, DB_NAME, DB_USER, DB_USER_PSWD, OTHERS, DB_NOTE)
 values (3, 'DBFAS', 'OLEDB', 'Oracle', 'DBDMS_bzkf', 'DBDMS', 'eSKBmbt0cx6F3jZr7+L9Qw==', '4bTNb3DN0cw=', 'needdecode;ischineselang', 'DBDMS');
 
-insert into database_list (DBID, DBALIAS, PROVIDER, DBTYPE, SERVERNAME, DBNAME, DBUSER, DBUSERPSWD, OTHERS, DBNOTE)
+insert into database_list (db_id, DB_ALIAS, PROVIDER, DB_TYPE, db_SERVER_NAME, DB_NAME, DB_USER, DB_USER_PSWD, OTHERS, DB_NOTE)
 values (4, 'DBSALE', 'OLEDB', 'Oracle', 'DBDMS_bzkf', 'DBDMS', 'bfWHC8T3dfKsXHKKB/94tQ==', 'QvvpwaOFpjM=', 'needdecode;ischineselang', 'DBDMS');
-
